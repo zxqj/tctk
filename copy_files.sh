86aa27e29078cc8f35613e5f2bda7ca5710c8712
Patrick Branch <branch.patrick@gmail.com>
2026-04-08 03:55:30 -0400

    removed unnecessary file

--- copy_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-src="/src/tctk-stable-old"
-dst="/src/tctk"
-
-find "$src" -type f \( -name "*.sh" -o -name "*.yaml" -o -name "*.toml" -o -name "*.py" -o -name "*.json" -o -name "*.md" \) ! -path "*/.git/*" | while read -r file; do
-    rel="${file#$src/}"
-    mkdir -p "$dst/$(dirname "$rel")"
-    cp -v "$file" "$dst/$rel"
-done
